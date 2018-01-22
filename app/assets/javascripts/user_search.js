ec0b4f3163f0bfdf5ec1f64effe4eda0258afcbd
dcabj <futbolvalent15@gmail.com>
2018-01-22 18:32:47 +0900

    [feature]　 # Add Ajax function about incremental search of user. # Add js to add user selected in incremental search to input tag in form

---
+++ app/assets/javascripts/user_search.js
@@ -0,0 +1,75 @@
+$(document).on('turbolinks:load', function(){
+  function buildSearchUserResult(user){
+    var html = `<div id="chat-group-users">
+                  <div id="chat-group-user-${user.id}" class="chat-group-user clearfix">
+                    <p class="chat-group-user__name">${user.user_name}</p>
+                    <a class="user-search-add chat-group-user__btn chat-group-user__btn--add js-add-btn" data-user-id="${user.id}" data-user-name="${user.user_name}">追加</a>
+                  </div>
+                </div>`
+    $('#user-search-result').append(html);
+  };
+
+  function registUserSet(id, name){
+    var html = `<div class="chat-group-user clearfix js-chat-member" id="chat-group-user-${id}">
+                    <input name="group[user_ids][]" type="hidden" value="${id}">
+                    <p class="chat-group-user__name">${name}</p>
+                    <a class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn" data-user-id="${id}" data-user-name="testman">削除</a>
+                  </div>`
+
+    $('.chat-group-users').append(html);
+
+  };
+
+  $("#user-search-field").on("keyup", function() {
+    var input = $("#user-search-field").val();
+    $.ajax({
+      type: 'GET',
+      url: '/users/search',
+      data: {name: input},
+      dataType:'json'
+    })
+    .done(function(users) {
+      $("#user-search-result").empty();
+      if($.isArray(users)){
+        var added_user = new Array();
+        $(".js-chat-member").each(function(i){
+          var id = $(".js-chat-member:eq("+i+") a")[0].getAttribute("data-user-id");
+          added_user.unshift(Number(id));
+        });
+        users.forEach(function(user){
+          if($.inArray(user.id, added_user) == -1){
+            buildSearchUserResult(user)
+          };
+        });
+      }else{
+        return false;
+      }
+    })
+    .fail(function(){
+      alert("ユーザーを検索できませんでした")
+    })
+  });
+
+  $(document).on("click", ".user-search-add", function(event){
+    var add_user = this
+    // $(".js-chat-member").each(function(i){
+    //   judge = true;
+    //   if (add_user.getAttribute('data-user-id') == $(".js-chat-member:eq("+i+") a")[0].getAttribute("data-user-id")){
+
+    //     judge = false;
+    //     return false;
+    //   }
+    // },add_user);
+    // if(!judge){
+    //   return false;
+    // }
+    user_id = this.getAttribute('data-user-id')
+    user_name = this.getAttribute('data-user-name')
+    registUserSet(user_id,user_name);
+    $(this).parent().parent().remove();
+  });
+
+  $(document).on("click", ".user-search-remove", function(){
+    $(this).parent().remove()
+  });
+});
---
+++ app/views/users/search.json.jbuilder
@@ -0,0 +1,6 @@
+if @users.present?
+  json.array! @users do |user|
+    json.id  user.id
+    json.user_name  user.name
+  end
+end
